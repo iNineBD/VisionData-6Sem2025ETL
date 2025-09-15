d4fd4280eee00b29fa7defe81a8db45bba33fb36
aoliveira <andreluiz1088@hotmail.com>
2025-09-15 19:31:28 -0300

    API-17-chore: remove unused imports from main.py and requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 # ruff: noqa: F401
 from schedule import run_pending
+<<<<<<< HEAD
 from process.scheduler import scheduler
 from config.logger import setup_logger
 from config.elastic_client import ElasticClient
+=======
+from process.scheduler import start_etl
+from config.logger import setup_logger
+>>>>>>> 5585ebd (API-17-chore: remove unused imports from main.py and requirements.txt)
 
 logger = setup_logger(__name__)
 
